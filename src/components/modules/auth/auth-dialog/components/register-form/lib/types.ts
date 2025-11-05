6d2ebc93fa95edd0b79c00bf6d9be980467abe3d
Nguyen Duy Bach <nguyenduybach12a11@gmail.com>
2025-11-06 00:17:28 +0700

    feat(auth): add step verify otp register

--- src/components/modules/auth/auth-dialog/components/register-form/lib/types.ts
+++ src/components/modules/auth/auth-dialog/components/register-form/lib/types.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
-import { REGISTER_FORM_SCHEMA } from "./constants";
+import {
+  REGISTER_FORM_SCHEMA,
+  REGISTER_STEP1_SCHEMA,
+  REGISTER_STEP2_SCHEMA,
+} from "./constants";
 
 export type RegisterFormValues = z.infer<typeof REGISTER_FORM_SCHEMA>;
+export type RegisterStep1Values = z.infer<typeof REGISTER_STEP1_SCHEMA>;
+export type RegisterStep2Values = z.infer<typeof REGISTER_STEP2_SCHEMA>;
 
 export interface Props {
   onSwitchViewMode: () => void;
 }
+
+export type RegistrationStep = 1 | 2;
