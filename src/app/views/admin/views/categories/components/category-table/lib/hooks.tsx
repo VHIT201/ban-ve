81468300a66a9acc4a35235f82b09ba078525b0c
Nguyen Duy Bach <nguyenduybach12a11@gmail.com>
2026-01-16 14:41:20 +0700

    fix: check before push

--- src/app/views/admin/views/categories/components/category-table/lib/hooks.tsx
+++ src/app/views/admin/views/categories/components/category-table/lib/hooks.tsx
@@ -21,28 +21,6 @@ export const useCategoryTableColumnsDefs = (
   return useMemo<ColumnDef<CategoryTableRow>[]>(
     () => [
       {
-        id: "select",
-        header: ({ table }) => (
-          <Checkbox
-            checked={table.getIsAllPageRowsSelected()}
-            onCheckedChange={(value) =>
-              table.toggleAllPageRowsSelected(!!value)
-            }
-            aria-label="Select all"
-          />
-        ),
-        cell: ({ row }) => (
-          <Checkbox
-            checked={row.getIsSelected()}
-            onCheckedChange={(value) => row.toggleSelected(!!value)}
-            aria-label="Select row"
-            onClick={(e) => e.stopPropagation()}
-          />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-      },
-      {
         accessorKey: "name",
         header: "Tên danh mục",
         cell: ({ row }) => {
