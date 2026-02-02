be882d5b026e84a1430fca0f222b81c8750f1e26
Nguyen Duy Bach <nguyenduybach12a11@gmail.com>
2026-02-03 05:23:19 +0700

    fix: pull new main

--- src/app/views/auth/views/register/components/register-form/lib/constants.ts
+++ src/app/views/auth/views/register/components/register-form/lib/constants.ts
@@ -5,25 +5,24 @@ export const REGISTER_FORM_SCHEMA = z
     name: z
       .string()
       .min(2, "Họ tên phải có ít nhất 2 ký tự")
-      .regex(
-        /^[\p{L}\s]+$/u,
-        "Họ tên không được chứa số hoặc ký tự đặc biệt"
-      ),
-    
+      .max(30, "Họ tên không được vượt quá 50 ký tự")
+      .regex(/^[\p{L}\s]+$/u, "Họ tên không được chứa số hoặc ký tự đặc biệt"),
+
     email: z.string().email("Email không hợp lệ"),
     password: z
       .string()
       .min(6, "Mật khẩu phải có ít nhất 6 ký tự")
+      .max(30, "Mật khẩu không được vượt quá 30 ký tự")
       .regex(
         /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
-        "Mật khẩu phải bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt"
+        "Mật khẩu phải bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt",
       ),
     confirmPassword: z
       .string()
       .min(6, "Xác nhận mật khẩu phải có ít nhất 6 ký tự")
       .regex(
         /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
-        "Mật khẩu phải bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt"
+        "Mật khẩu phải bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt",
       ),
   })
   .refine((data) => data.password === data.confirmPassword, {
