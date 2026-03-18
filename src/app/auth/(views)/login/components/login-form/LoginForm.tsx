532726f55eebec52292b8e735750d5db5fe00277
lenguyenhoang123 <lenguyenhoang9.10@gmail.com>
2026-03-18 15:26:14 +0700

    fix: re-enable password hashing on frontend

--- src/app/auth/(views)/login/components/login-form/LoginForm.tsx
+++ src/app/auth/(views)/login/components/login-form/LoginForm.tsx
@@ -34,6 +34,7 @@ import { PostApiAuthLogin200Data } from "@/api/models";
 import { extractErrorMessage } from "@/utils/error";
 import Link from "next/link";
 import { setCookie } from "@/utils/cookies";
+import { encodeSHA256 } from "@/utils/encode";
 
 const LoginForm: FC<Props> = () => {
   // State
@@ -69,7 +70,7 @@ const LoginForm: FC<Props> = () => {
       const loginResponse = await loginMutation.mutateAsync({
         data: {
           email: values.email,
-          password: values.password,
+          password: encodeSHA256(values.password),
         },
       });
 
